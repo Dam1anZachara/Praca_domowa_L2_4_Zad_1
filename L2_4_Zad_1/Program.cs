90733c08b2d54196288b6fc673cc2f62431ad855
damia <damia@DESKTOP-G1NUIKK>
2021-12-15 22:16:48 +0100

    Add project files.

---
+++ L2_4_Zad_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L2_4_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            byte age;
+            char gender;
+            string peselNumber;
+            uint workerNumber;
+        }
+    }
+}
